491aae372e9fa272a7e287ffb2b76421fc2ccff5
rainbown1 <milcacelestenavadedios@gmail.com>
2025-10-31 21:00:09 -0600

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.hilt.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
